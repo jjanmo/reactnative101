e495d98f469159d6bcd104e04cafe680e541ddae
jjanmo <jjanmo07@hanmail.net>
2021-10-02 17:00:03 +0900

    feat: add feature of getting current location

--- weather-app-2021/App.js
+++ weather-app-2021/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import {SafeAreaView, View, Text, StyleSheet} from 'react-native';
+import React, {useEffect} from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  PermissionsAndroid,
+} from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
 import Calendar from './src/components/Calender';
 import Weather from './src/components/Weather';
 import Location from './src/components/Location';
 
 const App = () => {
+  useEffect(async () => {
+    if (Platform.OS === 'ios') {
+      Geolocation.requestAuthorization('always');
+      Geolocation.setRNConfiguration({
+        skipPermissionRequests: false,
+        authorizationLevel: 'whenInUse',
+      });
+    } else if (Platform.OS === 'android') {
+      await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      );
+    }
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <Calendar />
--- weather-app-2021/src/components/Location.js
+++ weather-app-2021/src/components/Location.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
 
 const Location = () => {
+  const [location, setLocation] = useState({latitude: 0, longitude: 0});
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      position => {
+        console.log(position);
+        const {latitude, longitude} = position.coords;
+        setLocation({
+          latitude,
+          longitude,
+        });
+      },
+      error => {
+        console.log(error.code, error.message);
+      },
+    );
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.location}>Location</Text>
+      <Text style={styles.location}>{location.latitude}</Text>
+      <Text style={styles.location}>{location.longitude}</Text>
     </View>
   );
 };
