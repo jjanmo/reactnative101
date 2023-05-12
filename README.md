088a63e7f0ced637cf6d99a63bdad9ebf5835e4e
jjanmo <jjanmo07@hanmail.net>
2023-05-12 17:42:29 +0900

    fix: update readme and fix style

--- README.md
+++ README.md
@@ -1,23 +1,26 @@
 # React Native 101
 
-> Learn react native making weather app 🥳
+> Learn react native making simple apps 🥳
 
-> with [NomadCoders](https://nomadcoders.co/react-native-fundamentals/lobby)
+> with [NomadCoders ReactNative 101](https://nomadcoders.co/react-native-for-beginners)
 
-## App Flow
+## ver.2022
+
+> 업데이트되면 한번식 들춰보게된다. 👍
+
+## ver.2021 React Native 101
+
+<details>
+  <summary>Intro</summary>
+
+- 처음 위 강의를 통해서 리액트 네이티브를 시작하였다. 회사에서 리액트 네이티브를 사용해야했기 때문에 스스로 (선!)학습을 위해서 선택한 강의였다. 그렇게 1년이 지난 지금, 해당강의가 업데이트 되었다는 소식을 들었다. 업데이트된 부분이 무엇이 있는지 궁금해서 다시 한번 들춰본다.
+
+- 해당 강의는 Expo를 사용한다. 하지만 실제 리액트 네이티브 앱을 만들때는 대부분 `React-Native-CLI`를 통해서 만든다. 나 역시 실무에서는 후자를 사용하였다.
+
+</details>
+
+---
+
+## ver.2020 MyWeatherApp Flow
 
 ![app-flow](screenshots/weather_app.png)
-
-<br />
-
----
-
----
-
-<br />
-
-# 2021 Updated React Native 101
-
-> [노마드 코더의 리액트 네이티브 강의](https://nomadcoders.co/react-native-for-beginners)를 통해서 리액트 네이티브를 시작하였다. 회사에서 리액트 네이티브를 사용해야했기 때문에 스스로 (선!)학습을 위해서 선택한 강의였다. 그렇게 1년이 지난 지금, 해당강의가 업데이트 되었다는 소식을 들었다. 업데이트된 부분이 무엇이 있는지 찾아보고 스스로 Learning by Doing을 통해서 만들어보고 실습해보고자 한다.
-
-> 해당 강의는 Expo를 사용한다. 하지만 실제 리액트 네이티브 앱을 만들때는 대부분 React-Native-CLI를 통해서 만든다. 나 역시 실무에서는 후자를 사용하였다. 그래서 나는 해당 강의를 실습할 때, React-Native-CLI를 통해서 하고자 한다.
--- weather-app/App.js
+++ weather-app/App.js
@@ -8,50 +8,50 @@ import Weather from './components/Weather';
 const API_KEY = '2a4dfa6f9892a50c75182e50c8cc820d';
 
 export default class extends React.Component {
-    state = {
-        isLoading: true,
-    };
+  state = {
+    isLoading: true,
+  };
 
-    async getWeather(latitude, longitude) {
-        const {
-            data: {
-                main: { temp },
-                weather: [{ main: condition }],
-                name: city,
-                sys: { country },
-            },
-        } = await axios.get(
-            `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
-        );
+  async getWeather(latitude, longitude) {
+    const {
+      data: {
+        main: { temp },
+        weather: [{ main: condition }],
+        name: city,
+        sys: { country },
+      },
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
 
-        this.setState({
-            isLoading: false,
-            temp,
-            condition,
-            city,
-            country,
-        });
+    this.setState({
+      isLoading: false,
+      temp,
+      condition,
+      city,
+      country,
+    });
+  }
+
+  async getLocation() {
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+    } catch (e) {
+      Alert.alert('🚨Can not find Location🚨', 'So Sad 😭');
     }
+  }
 
-    async getLocation() {
-        try {
-            await Location.requestPermissionsAsync();
-            const {
-                coords: { latitude, longitude },
-            } = await Location.getCurrentPositionAsync();
-            this.getWeather(latitude, longitude);
-        } catch (e) {
-            Alert.alert('🚨Can not find Location🚨', 'So Sad 😭');
-        }
-    }
+  componentDidMount() {
+    this.getLocation();
+  }
 
-    componentDidMount() {
-        this.getLocation();
-    }
+  render() {
+    const { isLoading } = this.state;
 
-    render() {
-        const { isLoading } = this.state;
-
-        return isLoading ? <Loader /> : <Weather {...this.state} />;
-    }
+    return isLoading ? <Loader /> : <Weather {...this.state} />;
+  }
 }
