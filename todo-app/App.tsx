ba0e7cd71f6c7779f697fe96317cf3959b9d6577
jjanmo <jjanmo07@hanmail.net>
2023-08-13 23:51:47 +0900

    feat: add header of pressing-button

--- todo-app/App.tsx
+++ todo-app/App.tsx
@@ -1,20 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { theme } from './colors';
+import { useState } from 'react';
 
 export default function App() {
+  const [active, setActive] = useState<'work' | 'travel'>('work');
+
+  const handlePress = () => {
+    setActive((prev) => {
+      return prev === 'travel' ? 'work' : 'travel';
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={handlePress}>
+          <Text
+            style={{
+              ...styles.button,
+              color: active === 'work' ? theme.text : theme.inactiveText,
+            }}
+          >
+            Work
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handlePress}>
+          <Text
+            style={{
+              ...styles.button,
+              color: active === 'travel' ? theme.text : theme.inactiveText,
+            }}
+          >
+            Travel
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingHorizontal: 30,
     flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.background,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 20,
+  },
+  button: {
+    fontSize: 40,
   },
 });
---
+++ todo-app/colors.ts
@@ -0,0 +1,5 @@
+export const theme = {
+  background: '#000',
+  text: '#fff',
+  inactiveText: '#3a3d40',
+};
