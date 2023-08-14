8a33e8e1ffb243e7d779b59df0e069032c32ba5b
jjanmo <jjanmo07@hanmail.net>
2023-08-14 17:27:28 +0900

    feat: add todolist view

--- todo-app/App.styles.ts
+++ todo-app/App.styles.ts
@@ -22,9 +22,35 @@ export const styles = StyleSheet.create({
   input: {
     paddingVertical: 10,
     paddingHorizontal: 10,
-    marginVertical: 15,
+    marginVertical: 20,
     borderRadius: 5,
     backgroundColor: theme.white,
     fontSize: 20,
+  },
+  todo: {
+    paddingVertical: 6,
+    paddingHorizontal: 4,
+    marginVertical: 6,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#c7ecee',
+    borderRadius: 5,
+  },
+  checkbox: {
+    margin: 8,
+  },
+  todoText: {
+    fontSize: 18,
+  },
+  rowCenter: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  iconButton: {
+    width: 30,
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
--- todo-app/App.tsx
+++ todo-app/App.tsx
@@ -1,29 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
 import {
-  NativeSyntheticEvent,
-  StyleSheet,
+  ScrollView,
   Text,
   TextInput,
-  TextInputChangeEventData,
   TouchableOpacity,
   View,
 } from 'react-native';
-import { theme } from './colors';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { styles } from './App.styles';
+import { useAsyncStorage } from './hooks';
+import * as Crypto from 'expo-crypto';
+import Checkbox from 'expo-checkbox';
+import { Feather, Ionicons } from '@expo/vector-icons';
+
+type TodoType = 'work' | 'travel';
+interface Todo {
+  id: string;
+  type: TodoType;
+  content: string;
+  completed: boolean;
+}
+interface Todos {
+  [key: string]: Todo;
+}
 
 export default function App() {
-  const [active, setActive] = useState<'work' | 'travel'>('work');
+  const [currentType, setCurrentType] = useState<TodoType>();
+  const [todos, setTodos] = useState<Todos>({});
+
+  const { loadData, storeData } = useAsyncStorage();
+  useEffect(() => {
+    loadData('@currentType', 'work').then((data) => setCurrentType(data));
+    loadData('@todos', {}).then((data) => setTodos(data));
+  }, []);
+
   const [text, setText] = useState<string>('');
+  const handleChangeText = (text: string) => {
+    setText(text);
+  };
 
   const handlePress = () => {
-    setActive((prev) => {
+    setCurrentType((prev) => {
       return prev === 'travel' ? 'work' : 'travel';
     });
   };
-  const handleChangeText = (text: string) => {
-    console.log(text);
-    setText(text);
+
+  const handleSubmit = () => {
+    const key = Crypto.randomUUID();
+    const newTodo: Todo = {
+      id: key,
+      content: text,
+      type: currentType!,
+      completed: false,
+    };
+    setTodos({
+      ...todos,
+      [key]: newTodo,
+    });
+    setText('');
   };
+
+  useEffect(() => {
+    storeData('@currentType', currentType);
+    storeData('@todos', todos);
+    console.log(currentType, todos);
+  }, [currentType, todos]);
 
   return (
     <View style={styles.container}>
@@ -31,20 +72,20 @@ export default function App() {
       <View style={styles.header}>
         <TouchableOpacity onPress={handlePress}>
           <Text
-            style={{
-              ...styles.button,
-              color: active === 'work' ? theme.white : theme.grey,
-            }}
+            style={[
+              styles.headerButton,
+              currentType === 'work' ? styles.highlighted : null,
+            ]}
           >
             Work
           </Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={handlePress}>
           <Text
-            style={{
-              ...styles.button,
-              color: active === 'travel' ? theme.white : theme.grey,
-            }}
+            style={[
+              styles.headerButton,
+              currentType === 'travel' ? styles.highlighted : null,
+            ]}
           >
             Travel
           </Text>
@@ -54,36 +95,33 @@ export default function App() {
         <TextInput
           style={styles.input}
           placeholder={`${
-            active === 'work' ? 'Add To Do!' : 'Where do you want to go?'
+            currentType === 'work' ? 'Add To Do!' : 'Where do you want to go?'
           }`}
-          onChangeText={handleChangeText}
           value={text}
+          onChangeText={handleChangeText}
+          onSubmitEditing={handleSubmit}
         />
       </View>
+      <ScrollView>
+        {Object.keys(todos)
+          .filter((key) => todos[key].type === currentType)
+          .map((key) => (
+            <View key={key} style={styles.todo}>
+              <View style={styles.rowCenter}>
+                <Checkbox style={styles.checkbox} />
+                <Text style={styles.todoText}>{todos[key].content}</Text>
+              </View>
+              <View style={styles.rowCenter}>
+                <TouchableOpacity style={styles.iconButton} activeOpacity={0.6}>
+                  <Feather name="edit" size={20} color="grey" />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.iconButton} activeOpacity={0.6}>
+                  <Ionicons name="trash" size={20} color="grey" />
+                </TouchableOpacity>
+              </View>
+            </View>
+          ))}
+      </ScrollView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: 30,
-    flex: 1,
-    backgroundColor: theme.black,
-  },
-  header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginTop: 20,
-  },
-  button: {
-    fontSize: 40,
-  },
-  input: {
-    paddingVertical: 10,
-    paddingHorizontal: 10,
-    marginVertical: 15,
-    borderRadius: 5,
-    backgroundColor: theme.white,
-    fontSize: 20,
-  },
-});
