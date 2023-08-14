124d3adab8504c5b9a69e1ecddb7d06e9546e24f
jjanmo <jjanmo07@hanmail.net>
2023-08-14 22:39:07 +0900

    chores

--- expo-tutorial/src/screens/Home.tsx
+++ expo-tutorial/src/screens/Home.tsx
@@ -1,19 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import ImageViewer from '../components/ImageViewer';
 import Button from '../components/Button';
-import { FontAwesome } from '@expo/vector-icons';
+import * as ImagePicker from 'expo-image-picker';
 
 const bgImageSource = require('../../assets/background-image.png');
 
 export default function Home() {
+  const pickImage = async () => {
+    const getImage = async () => {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        quality: 1,
+      });
+      return result;
+    };
+
+    const result = await getImage();
+
+    console.log(result);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imageSource={bgImageSource} />
       </View>
       <View style={styles.buttonContainer}>
-        <Button label="Choose a button" theme="primary" />
-        <Button label="Use this photo" />
+        <Button label="Choose a photo" theme="primary" onPress={pickImage} />
+        <Button label="Use this photo" onPress={() => alert('pressing')} />
       </View>
     </View>
   );
